d0910eb1dfc1e80e68fdc6ff93a041360137c29c
Naynay Kaaay <naynaykaaay@Naynays-MacBook-Air.local>
2026-01-14 00:40:05 -0500

    cw

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,20 +1,5 @@
 
 
-function move() {
-  var elem = document.getElementById("myBar");
-  var width = 20;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-    } else {
-      width++;
-      elem.style.width = width + '%';
-      elem.innerHTML = width * 1  + '%';
-    }
-  }
-}
-
 const animals = document.getElementsByClassName("animals");
 
 
@@ -45,3 +30,19 @@ for (let i = 0; i < animals.length; i++) {
     moving(animals[i]);
 }
 
+
+
+function move() {
+    var elem = document.getElementById("myBar");
+    var width = 20;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++;
+        elem.style.width = width + '%';
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
