d966ec1191585ecf1a86956207469e65d919480c
Ashley Li <ali64@stuy.edu>
2026-01-13 23:21:59 -0500

    pop up + bar done

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,16 @@
 var animal.getElementById("animal");
+
+function move() {
+  var elem = document.getElementById("myBar");
+  var width = 20;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
